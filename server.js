b6b7ce7ab1240bdc4b4d30e9361aa8ca1b99af9d
Muhammad Abdullah Nabeel <baanwarapapiha@gmail.com>
2022-10-04 15:12:02 +0500

    gen request done

---
+++ server.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const app = express()
+const axios = require('axios');
+var parseString = require('xml2js').parseString;
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const pririty1 = ['com', 'net', 'org']
+const pririty2 = ['uk', 'dev']
+const pririty3 = ['edu', 'it']
+const pririty4 = ['any', 'el', 'info']
+
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+  let domains_list = [];
+  let pol_res = []
+  let api_res = []
+
+  // recieved this data
+  let priority = req.body.priority
+  let duration = req.body.duration
+  let url = req.body.url
+  const [domain, tld] = url.split('.')
+  // give object as info about input of request
+  var req_info = {
+    url: url,
+    domain: domain,
+    tld: tld,
+    priority: priority,
+    duration: duration
+  }
+
+  priority_name = "priority".concat(priority)
+  const add_to_list = (t) => domains_list.push(domain.concat(".", t))
+  pririty2.forEach(add_to_list);
+
+  let mod_priority = domains_list.toString()
+  let namecheap_uri = 
+  `https://api.sandbox.namecheap.com/xml.response?ApiUser=${process.env.ApiUser}&ApiKey=${process.env.ApiKey}&UserName=${process.env.UserNameNameCheap}&Command=namecheap.domains.check&ClientIp=${process.env.ClientIp}&DomainList=${mod_priority}`
+
+  try {
+    let response = await axios.get(namecheap_uri)
+    // console.log(response)
+    output = parseString(response.data, function (err, result) {
+      // console.dir(result);
+      api_res = result
+    });
+
+    if (api_res.ApiResponse.$.Status=='OK') {
+      let polish = api_res.ApiResponse.CommandResponse[0].DomainCheckResult
+      polish.forEach((unpol)=>{
+        pol_res.push(unpol['$'])
+      })
+  
+      namecheap_metadata = {
+        status: api_res.ApiResponse.$.Status,
+        errors: api_res.ApiResponse.Errors,
+        warnings: api_res.ApiResponse.Warnings,
+        command_requested: api_res.ApiResponse.RequestedCommand,
+        server: api_res.ApiResponse.Server,
+        gmt_time_diff: api_res.ApiResponse.GMTTimeDifference,
+        execution_time: api_res.ApiResponse.ExecutionTime
+      }
+  
+      res.status(200).json({
+        req_info: req_info,
+        namecheap_metadata: namecheap_metadata,
+        data: pol_res
+      })
+  
+    } else {
+      namecheap_metadata = {
+        status: api_res.ApiResponse.$.Status,
+        errors: api_res.ApiResponse.Errors,
+        warnings: api_res.ApiResponse.Warnings,
+        command_requested: api_res.ApiResponse.RequestedCommand,
+        server: api_res.ApiResponse.Server,
+        gmt_time_diff: api_res.ApiResponse.GMTTimeDifference,
+        execution_time: api_res.ApiResponse.ExecutionTime
+      }
+  
+      res.status(200).json({
+        req_info: req_info,
+        namecheap_metadata: namecheap_metadata,
+        // data: pol_res
+      })
+
+    }
+
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      error: error.message,
+    })
+  }
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`App listening on port ${process.env.PORT}`)
+})
