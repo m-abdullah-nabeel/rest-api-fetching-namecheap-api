e1fbe375dd8305dd23d261e3fa8e9cd613335b44
Muhammad Abdullah Nabeel <baanwarapapiha@gmail.com>
2022-10-07 01:12:47 +0500

    route working fine

--- domains/testImport.js
+++ domains/testImport.js
@@ -1,3 +1,2 @@
-// import myJson from './tld_priority_list.json' assert {type: 'json'};
 var myJson = require('./tld_priority_list.json');
 console.log(myJson.priority_0)
--- routes/price.js
+++ routes/price.js
@@ -1,19 +1,35 @@
 const express = require("express");
 const router = express.Router();
 var price_check = require("../price_check");
+var tld_priority = require('../domains/tld_priority_list.json');
 
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 
 router.get('/', async (req, res) => {
-    let sample_domains_list = ['net', 'org', 'uk', 'com', 'dev', 'bz', 'biz', 'co', 'xyz', 'me', 'io', 'me']
-    var actual = await price_check(sample_domains_list)
+    let [domain, tld] = req.body.url.split('.')
+    let priority = req.body.priority
+    let priority_key = `priority_${priority-1}`
+    if (tld_priority.hasOwnProperty(priority_key)) {
+        console.log("key found")
+    } else { console.log("Key not found")}
+    let priority_file = tld_priority[`priority_${priority-1}`]
+    // hasOwnProperty
+    if (priority==1) priority_file.unshift(tld)
+
+    // for testing keep this array here 
+    // let sample_domains_list = ['net', 'org', 'uk', 'com', 'dev', 'bz', 'biz', 'co', 'xyz', 'me', 'io', 'me']
+    var pricing_data = await price_check(priority_file)
   
     res.status(200).json({
-        data: actual,
+        domain:domain,
+        tld: tld,
+        priority: priority,
+        priority_file: priority_file,
+        pricing_data: pricing_data
     })
-  
+
 })
   
 module.exports = router;
