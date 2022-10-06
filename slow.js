87f1003495eecef00cca6b43c27285d8e4df34ab
Muhammad Abdullah Nabeel <baanwarapapiha@gmail.com>
2022-10-06 14:06:31 +0500

    highlighted needed info

--- slow.js
+++ slow.js
@@ -10,21 +10,43 @@ let pricingData = []
 let namecheap_uri = 
     `https://api.sandbox.namecheap.com/xml.response?ApiUser=${process.env.ApiUser}&ApiKey=${process.env.ApiKey}&UserName=${process.env.UserNameNameCheap}&ClientIp=${process.env.ClientIp}&Command=namecheap.users.getPricing&ProductType=DOMAIN&ProductName=vote`
 
-console.log(namecheap_uri)
+// console.log(namecheap_uri)
 
 axios.get(namecheap_uri)
 .then(response => {
 // console.log(response.data);
 parseString(response.data, function (err, result) {
-    console.dir(result);
-    // pricingData = result.ApiResponse.CommandResponse[0].UserGetPricingResult[0].ProductType[0].ProductCategory
+    // console.dir(result);
+    pricingData = result.ApiResponse.CommandResponse[0].UserGetPricingResult[0].ProductType[0].ProductCategory
     // console.dir(pricingData)
-    console.dir(result.ApiResponse.CommandResponse);
+    pricingData.map((i)=>{
+        let val = {
+            ActionName: i.$.Name,
+            tld: i.Product[0].$.Name,
+            data: i.Product[0].Price[0].$
+        }
+        console.log(val)
+        // console.log(i.$.Name)
+        // console.log(i.Product[0].$.Name)
+        // console.log(i.Product[0].Price[0].$)
+        // console.log("*******************************")
+        // console.log()
+    })
+
     console.log("*******************************")
-    // console.dir(result.ApiResponse.CommandResponse[0].UserGetPricingResult[0].ProductType[0].ProductCategory.length);
-    // console.dir(result.ApiResponse.CommandResponse[0].UserGetPricingResult[0].ProductType[0].ProductCategory[0].$);
+    console.log("*******************************")
+    // console.dir(result.ApiResponse.CommandResponse[0].UserGetPricingResult[0].ProductType[0].ProductCategory);
+    // console.log("*******************************")
+    console.dir(result.ApiResponse.CommandResponse[0].UserGetPricingResult[0].ProductType[0].ProductCategory[0].$);
+    // console.log("*******************************")
+    // console.dir(result.ApiResponse.CommandResponse[0].UserGetPricingResult[0].ProductType[0].ProductCategory[0].Product[0]);
+    console.log("*******************************")
+    console.dir(result.ApiResponse.CommandResponse[0].UserGetPricingResult[0].ProductType[0].ProductCategory[0].Product[0].$);
+    console.log("*******************************")
+    console.dir(result.ApiResponse.CommandResponse[0].UserGetPricingResult[0].ProductType[0].ProductCategory[0].Product[0].Price[0].$);
     // console.dir(result.ApiResponse.CommandResponse[0].UserGetPricingResult[0].ProductType[0].ProductCategory[0].Product);
-    console.dir(result.ApiResponse.Errors[0]);
+    // Errors
+    // console.dir(result.ApiResponse.Errors[0]);
     // console.dir(result.ApiResponse.Errors[0].Error);
 });
 
