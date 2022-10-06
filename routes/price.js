ae954e8e91c8e9c01c4644cbbcaf69ea4e772a72
Muhammad Abdullah Nabeel <baanwarapapiha@gmail.com>
2022-10-07 01:17:53 +0500

    added error handling

--- routes/price.js
+++ routes/price.js
@@ -11,24 +11,40 @@ router.get('/', async (req, res) => {
     let [domain, tld] = req.body.url.split('.')
     let priority = req.body.priority
     let priority_key = `priority_${priority-1}`
-    if (tld_priority.hasOwnProperty(priority_key)) {
-        console.log("key found")
-    } else { console.log("Key not found")}
-    let priority_file = tld_priority[`priority_${priority-1}`]
-    // hasOwnProperty
-    if (priority==1) priority_file.unshift(tld)
 
-    // for testing keep this array here 
-    // let sample_domains_list = ['net', 'org', 'uk', 'com', 'dev', 'bz', 'biz', 'co', 'xyz', 'me', 'io', 'me']
-    var pricing_data = await price_check(priority_file)
-  
-    res.status(200).json({
-        domain:domain,
-        tld: tld,
-        priority: priority,
-        priority_file: priority_file,
-        pricing_data: pricing_data
-    })
+    try {
+        if (tld_priority.hasOwnProperty(priority_key)) {
+            let priority_file = tld_priority[priority_key]
+            if (priority==1) priority_file.unshift(tld)
+            // for testing keep this array here 
+            // let sample_domains_list = ['net', 'org', 'uk', 'com', 'dev', 'bz', 'biz', 'co', 'xyz', 'me', 'io', 'me']
+            var pricing_data = await price_check(priority_file)
+          
+            res.status(200).json({
+                domain:domain,
+                tld: tld,
+                priority: priority,
+                pricing_data: pricing_data
+            })
+        
+        } else { 
+            res.status(200).json({
+                domain:domain,
+                tld: tld,
+                priority: priority,
+                error: "Invalid page or priority value"
+            })
+    
+        }
+    } catch (error) {
+        res.status(200).json({
+            domain:domain,
+            tld: tld,
+            priority: priority,
+            error: error.message
+        })
+
+    }
 
 })
   
