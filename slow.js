197180e5de300d1b1a709a01a0323dae79188d6e
Muhammad Abdullah Nabeel <baanwarapapiha@gmail.com>
2022-10-06 12:03:36 +0500

    correctly fteching results of one top level domain

--- slow.js
+++ slow.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 
+let pricingData = []
 // variable coincides with system variable UserName, used UserNameNameCheap
 let namecheap_uri = 
     `https://api.sandbox.namecheap.com/xml.response?ApiUser=${process.env.ApiUser}&ApiKey=${process.env.ApiKey}&UserName=${process.env.UserNameNameCheap}&ClientIp=${process.env.ClientIp}&Command=namecheap.users.getPricing&ProductType=DOMAIN&ProductName=vote`
@@ -15,10 +16,12 @@ axios.get(namecheap_uri)
 .then(response => {
 // console.log(response.data);
 parseString(response.data, function (err, result) {
-    // console.dir(result);
-    console.dir(result.ApiResponse.CommandResponse[0].UserGetPricingResult[0].ProductType[0].ProductCategory.length);
-    console.dir(result.ApiResponse.CommandResponse[0].UserGetPricingResult[0].ProductType[0].ProductCategory[0].$);
-    console.dir(result.ApiResponse.CommandResponse[0].UserGetPricingResult[0].ProductType[0].ProductCategory[0].Product);
+    console.dir(result);
+    pricingData = result.ApiResponse.CommandResponse[0].UserGetPricingResult[0].ProductType[0].ProductCategory
+    console.dir(pricingData)
+    // console.dir(result.ApiResponse.CommandResponse[0].UserGetPricingResult[0].ProductType[0].ProductCategory.length);
+    // console.dir(result.ApiResponse.CommandResponse[0].UserGetPricingResult[0].ProductType[0].ProductCategory[0].$);
+    // console.dir(result.ApiResponse.CommandResponse[0].UserGetPricingResult[0].ProductType[0].ProductCategory[0].Product);
     // console.dir(result.ApiResponse.Errors[0]);
     // console.dir(result.ApiResponse.Errors[0].Error);
     // console.dir(result.ApiResponse.CommandResponse[0].DomainCheckResult[0].$);
@@ -29,4 +32,3 @@ parseString(response.data, function (err, result) {
 .catch(error => {
 console.log(error);
 });
-
