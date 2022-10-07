47feff69eab1ef4b63ee6bca04f7f97de8126c93
Muhammad Abdullah Nabeel <baanwarapapiha@gmail.com>
2022-10-07 12:38:56 +0500

    removed template file

--- controllers/check.js
+++ controllers/check.js
@@ -5,16 +5,19 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 
+// this route cannot be checked by status f request because the Namecheap Error code sometimes fetches correct information with it
+// Say we have 20 listed domains, and one domain is not found, then error will be the status
+// but for other domains the availability check will be done correctly
+
 const checkApi = async (domain, tldomains) => {
-    let res_json = {}
     let pol_res = []
     let api_res = []
     let mod_priority = tldomains.map(x => domain.concat(".",x)).toString()
-    // let mod_priority = domains.toString()
+
     let namecheap_uri = 
     `https://api.sandbox.namecheap.com/xml.response?ApiUser=${process.env.ApiUser}&ApiKey=${process.env.ApiKey}&UserName=${process.env.UserNameNameCheap}&Command=namecheap.domains.check&ClientIp=${process.env.ClientIp}&DomainList=${mod_priority}`
+    // console.log(namecheap_uri)
 
-    console.log(namecheap_uri)
     try {
       let response = await axios.get(namecheap_uri)
       output = parseString(response.data, function (err, result) { api_res = result});
@@ -33,52 +36,14 @@ const checkApi = async (domain, tldomains) => {
         gmt_time_diff: api_res.ApiResponse.GMTTimeDifference,
         execution_time: api_res.ApiResponse.ExecutionTime
       }
-      res_json = {
+      return {
           namecheap_metadata: namecheap_metadata,
           data: pol_res
       }
-      return res_json
-
-    //   if (api_res.ApiResponse.$.Status=='OK') {
-    //     let polish = api_res.ApiResponse.CommandResponse[0].DomainCheckResult
-    //     polish.forEach((unpol)=>{
-    //       pol_res.push(unpol['$'])
-    //     })
-    
-    //     namecheap_metadata = {
-    //       status: api_res.ApiResponse.$.Status,
-    //       errors: api_res.ApiResponse.Errors,
-    //       warnings: api_res.ApiResponse.Warnings,
-    //       command_requested: api_res.ApiResponse.RequestedCommand,
-    //       server: api_res.ApiResponse.Server,
-    //       gmt_time_diff: api_res.ApiResponse.GMTTimeDifference,
-    //       execution_time: api_res.ApiResponse.ExecutionTime
-    //     }
-    //     res_json = {
-    //         namecheap_metadata: namecheap_metadata,
-    //         data: pol_res
-    //     }
-    //     return res_json
-    
-    //   } else {
-    //     namecheap_metadata = {
-    //       status: api_res.ApiResponse.$.Status,
-    //       errors: api_res.ApiResponse.Errors,
-    //       warnings: api_res.ApiResponse.Warnings,
-    //       command_requested: api_res.ApiResponse.RequestedCommand,
-    //       server: api_res.ApiResponse.Server,
-    //       gmt_time_diff: api_res.ApiResponse.GMTTimeDifference,
-    //       execution_time: api_res.ApiResponse.ExecutionTime
-    //   }
-
-    //   return namecheap_metadata
-    // }
-  
     } catch (error) {
-        res_json = {
+        return {
             error: error.message,
         }
-        return res_json
     }
 
 }
